dd630e90e5997757f00d115f10c5716ae8a8258b
taki0412 <taki041210@naver.com>
2022-01-21 11:48:36 +0900

    [FIX] Path

--- Faster_RCNN/parameter.py
+++ Faster_RCNN/parameter.py
@@ -1,7 +1,5 @@
-import os
-
 # Path
-ROOT_DIR = '/Users/taki0412/Programing/Repository/the-peach/wheat-head-detection'
+ROOT_DIR = '여기에 당신의 wheat-head-detection 파일의 절대 경로를 넣어주세요 !'
 DATA_DIR = f'{ROOT_DIR}/Faster_RCNN/data'
 TRAIN_DIR = f'{ROOT_DIR}/Faster_RCNN/data/train'
 TEST_DIR = f'{ROOT_DIR}/Faster_RCNN/data/test'
