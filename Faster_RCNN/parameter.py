edb269963766008d77f4fc4c88ea010a31d89815
taki0412 <taki041210@naver.com>
2022-01-26 10:26:00 +0900

    [FIX] Parameter

--- Faster_RCNN/parameter.py
+++ Faster_RCNN/parameter.py
@@ -3,6 +3,6 @@ import os
 # Path
 ROOT_DIR = f'{os.getcwd()}'
 DRIVE_DIR = f'{ROOT_DIR}/drive/MyDrive'
-DATA_DIR = f'{ROOT_DIR}/Faster_RCNN/data'
-TRAIN_DIR = f'{ROOT_DIR}/Faster_RCNN/data/train'
-TEST_DIR = f'{ROOT_DIR}/Faster_RCNN/data/test'
+DATA_DIR = f'{ROOT_DIR}/wheat-head-detection/Faster_RCNN/data'
+TRAIN_DIR = f'{ROOT_DIR}/wheat-head-detection/Faster_RCNN/data/train'
+TEST_DIR = f'{ROOT_DIR}/wheat-head-detection/Faster_RCNN/data/test'
